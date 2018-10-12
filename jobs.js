fe8486222daa9db7e901b70d949367b5f1bf39d4
Aditi Green <aditigreen@aditis-mbp.home>
2018-10-12 00:01:48 -0400

    added js and json

---
+++ jobs.js
@@ -0,0 +1,30 @@
+let  jobsJSON = [
+    {title: "DevOps Engineer",
+    company:"Ladders Inc.",
+    location: "New York, NY",
+    description: "Deploying and configuring production systems, with a particular focus on ease of configuration, reduction of human error, repeatability, and security."
+    },
+    {
+     title: "Software Engineer in Test",
+     comapany: "Ladders Inc.",
+     location: "New York, NY ",
+     description: "Participate in the requirements definition, design and development of a robust, extensible test harness for the company's functional test automation."
+    },
+    {
+     title: "Software Engineer (C#, WPF/MVVM, ASP.NET/MVC, Angular)",
+     company: "Skadden Arps",
+     location: "New York, NY",
+     description: "Creates databases and application interfaces based on analysis of requirements within established guidelines."
+    },
+    {
+    title: "IT / Enterprise : Senior Windows Systems Engineer",
+    company: "Confidential Company",
+    location: "New York, NY",
+    desciption: "Searching for a (UI + Drupal ) - Technical Lead / Architect with at least 4 years of experience in Drupal development and administration - theming, module building, core updates etc."
+  },{
+      title: "Full Stack Java / UI Software Engineer - Treasury Services",
+      company:"JPMorgan Chase",
+      location: "Jersey City, NJ",
+      description: "The Treasury Services team is seeking a Full Stack Java / UI Software Engineer. This individual will work with a team that develops smart and scalable solutions to provide a solid experience for our users"
+  }
+]
